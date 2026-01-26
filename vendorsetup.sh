89d02925e3e9cee3eb3bb904518a14358a39bc20
praveenxsharma <itachisec@duck.com>
2026-01-26 22:45:15 +0530

    Revert "Aerodactyl: Add infrastructure to auto-apply patches"
    This reverts commit ba60412600e527eff280fbbee8fa9f18256e01d6.

--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-deviceDir=$(gettop)/device/nothing/Aerodactyl/
-
-# apply patches
-${deviceDir}/applyPatches.sh ${deviceDir}/patches
-
-# For now, just skip the ABI checks to fix build errors.
-export SKIP_ABI_CHECKS=true
