6e89faa1cfcf23e11dd67308ff74f350e2268c13
praveenxsharma <sharmaps1756c@gmail.com>
2026-02-28 13:43:46 +0530

    Aerodactyl: initial bringup for Mist OS-QPR2

--- lineage_Pacman.mk
+++ lineage_Pacman.mk
@@ -24,3 +24,54 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=Pacman \
     SystemDevice=Pacman \
     SystemName=Pacman
+
+# MistOS
+# UDFPS support
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Pixel Launcher by default
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+
+# Custom package installer
+TARGET_USE_CUSTOM_PACKAGE_INSTALLER := true
+
+# Live wallpapers
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# Now Playing
+TARGET_SUPPORTS_NOW_PLAYING := true
+
+# Clear Calling
+TARGET_SUPPORTS_CLEAR_CALLING := true
+
+# Call Recording Support
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+# Bypass charging
+BYPASS_CHARGE_SUPPORTED := false
+
+# GMS
+WITH_GMS := true
+
+# Mist OS Flags
+MISTOS_MAINTAINER := Praveen_Sharma
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+#Some other stuff
+TARGET_DISABLE_EPPE := true
+PERF_ANIM_OVERRIDE := true
+
+#GPU
+GPU_FREQS_PATH := /devices/platform/soc/13000000.mali/devfreq/13000000.mali/available_frequencies
+GPU_MIN_FREQ_PATH := /devices/platform/soc/13000000.mali/devfreq/13000000.mali/min_freq
